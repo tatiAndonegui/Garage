1f78724166dfa80240aec5a9490a74e59c5fa34b
tatiAndonegui <115169557+tatiAndonegui@users.noreply.github.com>
2022-11-19 18:33:29 -0300

    enviromental

---
+++ Garage/src/Garage.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Garage {
+
+    private int masimunSizeOfTheGarage;
+    private int currentAumountOfVehicles;
+    private double wheelChangePrice;
+    private ArrayList<Vehicles> vehicles;
+
+    public void vehicleEntry(ArrayList<Vehicles> vehicles){
+        this.vehicles = vehicles;
+    }
+
+
+
+}
